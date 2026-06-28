dc96f65894d938d22e563d98ff1e3a5aa98c459e
sfaaud2682-sketch <sfaaud2682@gmail.com>
2026-06-28 05:22:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
 import os
-from telegram.ext import Application
-
-TOKEN = "8821925270:AAEqt1jatqhrgQH6p8LTffD1av_LGArb0os"
-
-def main():
-    print("🚀 ... يعمل الآن بكامل الصلاحيات والميزات (Vira) بوت فيرا")
-    application = Application.builder().token(TOKEN).build()
-    application.run_polling()
-
-if __name__ == '__main__':
-    main()
+TOKEN = os.getenv("TELEGRAM_TOKEN")
